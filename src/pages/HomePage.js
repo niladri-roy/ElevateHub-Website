3683ebd0e986a891cb7be9861787b0604a3f85cb
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-11-13 00:06:38 +0530

    Fix typos and simplify API calls in HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -20,10 +20,10 @@ const HomePage = () => {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
 
-  // get all cat
+  //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/get-category`);
+      const { data } = await axios.get("/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -36,27 +36,23 @@ const HomePage = () => {
     getAllCategory();
     getTotal();
   }, []);
+  //get products
+  const getAllProducts = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      setLoading(false);
+      setProducts(data.products);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
-  useEffect(() => {
-    const getAllProducts = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`);
-        setLoading(false);
-        setProducts(data.products);
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
-    };
-
-    if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length, page]);
-
-  // get total count
+  //getTOtal COunt
   const getTotal = async () => {
     try {
-      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-count`);
+      const { data } = await axios.get("/api/v1/product/product-count");
       setTotal(data?.total);
     } catch (error) {
       console.log(error);
@@ -65,20 +61,20 @@ const HomePage = () => {
 
   useEffect(() => {
     if (page === 1) return;
-    const loadMore = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`);
-        setLoading(false);
-        setProducts([...products, ...data?.products]);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
-
     loadMore();
-  }, [page, products]);
+  }, [page]);
+  //load more
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   // filter by cat
   const handleFilter = (value, id) => {
@@ -90,42 +86,26 @@ const HomePage = () => {
     }
     setChecked(all);
   };
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
 
   useEffect(() => {
-    if (!checked.length || !radio.length) {
-      const getAllProducts = async () => {
-        try {
-          setLoading(true);
-          const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`);
-          setLoading(false);
-          setProducts(data.products);
-        } catch (error) {
-          setLoading(false);
-          console.log(error);
-        }
-      };
-
-      getAllProducts();
-    }
-  }, [checked.length, radio.length, page]);
-
-  useEffect(() => {
-    if (checked.length || radio.length) {
-      const filterProduct = async () => {
-        try {
-          const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/product/product-filters`, {
-            checked,
-            radio,
-          });
-          setProducts(data?.products);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-
-      filterProduct();
-    }
+    if (checked.length || radio.length) filterProduct();
   }, [checked, radio]);
+
+  //get filterd product
+  const filterProduct = async () => {
+    try {
+      const { data } = await axios.post("/api/v1/product/product-filters", {
+        checked,
+        radio,
+      });
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Layout title={"All Products - Best offers "}>
